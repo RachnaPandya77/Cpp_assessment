f220f52ccd2701404bc543477fdddcbc56a60078
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-09-17 21:15:22 +0530

    add new task

--- oop_assessment.cpp
+++ oop_assessment.cpp
@@ -1,3 +1,15 @@
+/*Define a class to represent lecture details. Include the following members and the
+program should handle at least details of 5 lecturers.
+Data members:
+a) Name of the lecturer
+b) Name of the subject
+c) Name of course
+d) Number of lecturers
+Data functions:
+a) To assign initial values
+b) To add a lecture details
+c) To display name and lecture details */
+
 #include <iostream>
 using namespace std;
 
