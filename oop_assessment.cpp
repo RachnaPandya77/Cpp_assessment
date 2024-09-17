b3a9ce3291275ca9a20e59c876c0eed48e9573ae
Rachna Pandya <ruchipandya20@gmail.comgit config --global user.email ruchipandya20@gmail.com>
2024-09-17 23:03:58 +0530

    add new task

--- oop_assessment.cpp
+++ oop_assessment.cpp
@@ -13,8 +13,64 @@ c) To display name and lecture details */
 #include <iostream>
 using namespace std;
 
+class lectureDetail
+{
+public:
+    string lectureName;
+    string subjectName;
+    string courseName;
+    int numOfLecture;
+
+    // constructor for values
+    lectureDetail(string lecture, string subject, string course, int lectureNum)
+    {
+        lectureName = lecture;
+        subjectName = subject;
+        courseName = course;
+        numOfLecture = lectureNum;
+    }
+
+    // Function -- add lecture detail
+    void Add_Lecture_Detail(string lecture, string subject, string course, int lectureNum)
+    {
+        lectureName = lecture;
+        subjectName = subject;
+        courseName = course;
+        numOfLecture = lectureNum;
+    }
+
+    // Function --  display name and lecture detail
+    void Display_details()
+    {
+        cout << "lecturer Name: " << lectureName << endl;
+        cout << "Subject Name: " << subjectName << endl;
+        cout << "Course Name: " << courseName << endl;
+        cout << "Number Of Lecture: " << numOfLecture << endl;
+        cout << endl;
+    }
+};
+
 int main()
 {
-    cout << "Hello";
+    cout << "--------DETAILS  OF  LECTURE--------" << endl;
+    cout << endl;
+
+    lectureDetail lectures[5];
+
+    // initialize value -- using constructor
+    lectures[0] = lectureDetail("Jaydeep sir", "OOP Concept", "SE", 50);
+    lectures[1] = lectureDetail("Snehal Ma'am", "C language", "BCA", 40);
+    lectures[2] = lectureDetail("Nikhil Sir", "HTML", "BCA-2", 45);
+    lectures[3] = lectureDetail("Vaishali Ma'am", "Economics", "BA", 40);
+    lectures[4] = lectureDetail("Jagruti Ma'am", "Mathematics", "B.Com", 55);
+
+    // display lecture detail -- for loop
+
+    for (int i = 0; i < 5; i++)
+    {
+        lectures[i].Display_details();
+        cout << endl;
+    }
+
     return 0;
 }
